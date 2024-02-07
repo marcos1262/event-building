325da15e52971b18ec4b7cb48b41bf6bbf662c1c
Marcos Santos <marcospaulo1262@gmail.com>
2024-02-07 14:32:36 -0300

    Initial Commit

---
+++ EventBuilding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EventBuilding
+//
+//  Created by Marcos Santos on 07/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EventBuilding/EventBuildingApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventBuildingApp.swift
+//  EventBuilding
+//
+//  Created by Marcos Santos on 07/02/24.
+//
+
+import SwiftUI
+
+@main
+struct EventBuildingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
